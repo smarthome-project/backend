f37b87747a84c41ca4c528d51879cfa6489ef2a6
Unknown <przemsylaw.jozwik@aegton.com>
2018-01-03 22:49:04 +0100

    cold warm led validation

--- models/devices.js
+++ models/devices.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, Sequelize) => {
 	const DevicesAcceptedStatesKeys = {
 		"POWER" :['active'],
 		"LEDRGB": ['rgb'],
-		"LEDCW": ['rgb']
+		"LEDCW": ['cw']
 	}
 
 	function activeCheck(value) {
@@ -14,9 +14,14 @@ module.exports = (sequelize, Sequelize) => {
 		return /^#[0-9A-F]{6}$/i.test(value)
 	}
 
+	function checkCW(value) {
+		return /^#[0-9A-F]{4}00$/i.test(value)
+	}
+
 	const stateKeyValidator = {
 		'active': activeCheck,
-		'rgb': checkRGB
+		'rgb': checkRGB,
+		'cw': checkCW
 	}
 
 	const Devices = sequelize.define('devices', {
@@ -54,7 +59,9 @@ module.exports = (sequelize, Sequelize) => {
 					if ((DevicesAcceptedStatesKeys[this.type]).length == keys.length ){
 						keys.forEach( (key) => {
 							let func = stateKeyValidator[key]
-							if(!func || !func(this.state[key])){
+							if (!findKeyinArray(key, (DevicesAcceptedStatesKeys[this.type]))) {
+								throw new Error(`key ${key} is not accepted for type ${this.type}`)
+							} else if (!func || !func(this.state[key])){
 								throw new Error(`value ${this.state[key]} for state key ${key} is not accepted`)
 							}
 						})
@@ -67,6 +74,15 @@ module.exports = (sequelize, Sequelize) => {
 			}
 		}
 	})
+
+	const findKeyinArray = function(key,array){
+		for (var i = 0; i < array.length; i++) {
+			if (array[i] == key)
+				return true
+		}
+			return false
+	}
+
 
   return Devices
 }
